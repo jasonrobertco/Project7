f0857d16eb022f085cbcdd9ffaa6fa19588beadf
Jason Co <jasonrobertco@gmail.com>
2026-03-13 13:47:02 -0700

    done

--- game_survival.cpp
+++ game_survival.cpp
@@ -48,6 +48,7 @@ struct Cell
 ///////////////////////////////////////////////////////////////////////////
 // Class Definitions
 ///////////////////////////////////////////////////////////////////////////
+class Maze;
 
 class Player
 {
@@ -161,8 +162,8 @@ bool Maze::hasActiveSwitch() const
     // TODO check if any Switch cells are activated
     // Return true if so and false otherwise
     for (int row = 0; row < m_rows; row++){ //looop thru row
-        for (int col = 0; col < m_rows; col++){ //loop thru col
-            if (m_grid[row][col].type == CellType::SWITCH && m_grid[r][c].activated){
+        for (int col = 0; col < m_cols; col++){ //loop thru col
+            if (m_grid[row][col].type == CellType::SWITCH && m_grid[row][col].activated){
                 return true;
             }
         }
@@ -220,34 +221,208 @@ void Maze::display(const Player *player, const string &msg) const
 
 void Player::move(char dir)
 {
+    /*
+    Calculate the target coordinates based on the input direction (’n’, ’s’, ’e’, ’w’). You must verify that
+the move is within the maze bounds and that the destination is not a WALL or a BLOCK. Furthermore,
+an EXIT cell acts as an impassable barrier unless hasActiveSwitch() returns true. If the move is
+valid, update the player’s position and subtract the MOVE COST. Finally, handle cell-specific triggers:
+if the player enters a TRAP, deduct the TRAP PENALTY; if the player reaches a powered EXIT, the game
+ends in success*/
     // TODO: Implement move
-    char a = dir;
-    return;
+    //char a = dir;
+    //row and column direction
+    int dr = 0, dc = 0;
+    if (dir == 'n')
+        dr = -1;
+    else if (dir == 's')
+        dr = 1;
+
+    else if (dir == 'e')
+        dc = 1;
+    else if (dir == 'w')
+        dc = -1;
+    else
+        return;
+
+    //new position
+    int newRow = m_row + dr;
+    int newCol = m_col + dc;
+
+    //bounds check
+    if (!m_maze->isInBounds(newRow, newCol))
+        return;
+
+    //cell reference
+    Cell& target = m_maze->at(newRow, newCol);
+
+    //check wall or block
+    if (target.type == CellType::WALL || target.type == CellType::BLOCK)
+        return;
+
+    //check locked exit
+    if (target.type == CellType::EXIT && !m_maze->hasActiveSwitch())
+        return;
+
+    //if pass change to new position update movepoints by 1
+    m_row = newRow;
+    m_col = newCol;
+    m_points -= MOVE_COST;
+
+    //cell type = trap
+    if (target.type == CellType::TRAP)
+        m_points -= TRAP_PENALTY;
+
+    //cell type is euqal to exit and 1 active switch is on
+    if (target.type == CellType::EXIT && m_maze->hasActiveSwitch())
+    {
+        cout << "SUCCESS! Exit powered. Final Points: " << m_points << endl;
+        exit(0);
+    }
 }
 
 void Player::push(char dir)
 {
     // TODO: Implement push block
-    char a = dir;
-    return;
+    /*
+    void push(char dir);
+Check if a BLOCK is adjacent to the player in the given direction. If the cell immediately beyond that
+block is also within bounds and EMPTY, move the block to that target cell and move the player into the
+block’s former spot. This action is physically demanding and costs 1 point (ACTION COST) in addition
+to the standard movement cost.
+*/
+    //direction code repeated from above
+    int dr = 0, dc = 0;
+    if (dir == 'n')
+        dr = -1;
+    else if (dir == 's')
+        dr = 1;
+    else if (dir == 'e')
+        dc = 1;
+    else if (dir == 'w')
+        dc = -1;
+    else
+        return;
+
+    //new block code repeated from above
+    int blockRow = m_row + dr;
+    int blockCol = m_col + dc;
+
+    //find block behind current block
+    int targetRow = blockRow + dr;
+    int targetCol = blockCol + dc;
+
+    //checkbounds if either are in bounds fo rmaze
+    if (!m_maze->isInBounds(blockRow, blockCol) || !m_maze->isInBounds(targetRow, targetCol))
+        return;
+
+    //in front block has to be a block
+    if (m_maze->at(blockRow, blockCol).type != CellType::BLOCK)
+        return;
+
+    //in front block cannot be empty
+    if (m_maze->at(targetRow, targetCol).type != CellType::EMPTY)
+        return;
+
+    //make the new target (behind) a block make the old empty so that the block was pusedh
+    m_maze->at(targetRow, targetCol).type = CellType::BLOCK;
+    m_maze->at(blockRow, blockCol).type = CellType::EMPTY;
+
+    //move palyer
+    m_row = blockRow;
+    m_col = blockCol;
+
+    //calculate aciton cost
+    m_points -= (MOVE_COST + ACTION_COST);
 }
 
 void Player::pull(char dir)
 {
     // TODO: Implement pull block
-    char a = dir;
-    return;
+    /*
+    void pull(char dir);
+Check if the cell in the given direction is EMPTY and if there is a BLOCK located directly behind the
+player (the opposite direction). If both are true, the player moves forward and the block is ”dragged”
+into the player’s previous coordinates. This action costs 1 point (ACTION COST) in addition to the
+movement cost.
+    */
+   //same movmeent code
+    int dr = 0, dc = 0;
+    if (dir == 'n')
+        dr = -1;
+    else if (dir == 's')
+        dr = 1;
+    else if (dir == 'e')
+        dc = 1;
+    else if (dir == 'w')
+        dc = -1;
+    else
+        return;
+    //new postion
+    int newRow = m_row + dr;
+    int newCol = m_col + dc;
+    //target pull direction
+    int blockRow = m_row - dr;
+    int blockCol = m_col - dc;
+    //check bounds
+    if (!m_maze->isInBounds(newRow, newCol) || !m_maze->isInBounds(blockRow, blockCol))
+        return;
+    //check if its not empty
+    if (m_maze->at(newRow, newCol).type != CellType::EMPTY)
+        return;
+    //check if its ot a block
+    if (m_maze->at(blockRow, blockCol).type != CellType::BLOCK)
+        return;
+    //if conditiosn satisfies update the new pulled position to block and remove the old block set to epty
+    m_maze->at(m_row, m_col).type = CellType::BLOCK;
+    m_maze->at(blockRow, blockCol).type = CellType::EMPTY;
+    //update the players position
+
+    m_row = newRow;
+    m_col = newCol;
+    //update aciton cost
+    m_points -= (MOVE_COST + ACTION_COST);
 }
 
 void Player::collect()
 {
     // TODO: Implement collect treasure
+
+    /*
+    Check the player’s current cell. If the cell type is TREASURE, change the cell type to EMPTY and increase
+the player’s points by the TREASURE REWARD.
+    */
+
+    //call at on m_maze
+    //if cell type is treasure
+    if (m_maze->at(m_row, m_col).type == CellType::TREASURE)
+    {
+        //set cell type to empty
+        m_maze->at(m_row, m_col).type = CellType::EMPTY;
+        //change players points by treasure reward
+        m_points += TREASURE_REWARD;
+    }
     return;
 }
 
 void Player::activate()
 {
     // TODO: Implement active switch
+
+    /*
+    void activate();
+Check the player’s current cell. If the cell type is a SWITCH, toggle its activated boolean state (flipping
+it from true to false or vice versa). This action costs the player 1 point (ACTION COST).
+    */
+   //if cell type = switch 
+   if (m_maze->at(m_row, m_col).type == CellType::SWITCH)
+   {
+        //toggle to opposite state
+        //.activated cell variable
+        m_maze->at(m_row, m_col).activated = !m_maze->at(m_row, m_col).activated;
+        //action point cost 1
+        m_points -= ACTION_COST;
+   }
+
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -260,11 +435,14 @@ Game::Game(int rows, int cols)
     int startR = 1, startC = 1;
     // TODO: Uncomment the following line
     // Once you have implemented the constructor for maze
-    // m_maze->generateRandom(startR, startC);
+    m_maze->generateRandom(startR, startC);
+    //pointer mmaze to call generate rando funciton passing startR/C
+
     // TODO: Change this call to Player constructor to
     // use the default value
     // Right now negative score to end game
-    m_player = new Player(m_maze, startR, startC, -10);
+    //m_player = new Player(m_maze, startR, startC, -10);
+    m_player = new Player(m_maze, startR, startC);
 }
 
 Game::~Game()
